a3bd677da828d5481b3ee0856c7805847341c4f2
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-09-27 08:48:53 +0900

    tools prestart

--- app.cmd
+++ app.cmd
@@ -1,6 +1,6 @@
-call npm install
 @rem set DEBUG=app:*,express:*
 set DEBUG=app:*,express:application,express:router
+node tools/prestart
 start npm start
 start http://localhost:3000
 pause
---
+++ tools/prestart.js
@@ -0,0 +1,26 @@
+	'use strict';
+	const pkg = require('../package')
+	try {
+		Object.keys(pkg.dependencies).map(require);
+	} catch (e) {
+		//require('child_process').execSync('npm install');
+
+		const spawn = require('child_process').spawn,
+			child = process.platform === 'win32' ?
+				spawn('cmd', ['/c', 'npm', 'install'], {stdio: 'inherit'}) :
+				spawn('npm', ['install'], {stdio: 'inherit'});
+
+		//child.stdout && child.stdout.on('data', function (data) {
+		//	console.log('stdout: ' + data);
+		//});
+
+		//child.stderr && child.stderr.on('data', function (data) {
+		//	console.log('stderr: ' + data);
+		//});
+
+		child.on('close', function (code) {
+			console.log('child process exited with code ' + code);
+		});
+
+	}
+
