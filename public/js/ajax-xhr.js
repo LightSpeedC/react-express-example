0d601f9d1c48de47d9510d94bccfb8376da9aac4
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-09-30 07:35:07 +0900

    ajax

--- public/js/ajax-xhr.js
+++ public/js/ajax-xhr.js
@@ -1,23 +1,10 @@
 // http://js-next.hatenablog.com/entry/2013/11/28/093230
 // https://ja.wikipedia.org/wiki/XMLHttpRequest
 
-function ajaxGet(url) {
+function ajax(method, url, data) {
 	return new Promise(function (resolve, reject) {
 		var xhr = new XMLHttpRequest;
-		xhr.open('GET', url);
-		xhr.onload = function () {
-			if (xhr.status == 200) resolve(xhr.response);
-			else reject(new Error(xhr.statusText));
-		}
-		xhr.onerror = reject;
-		xhr.send();
-	});
-}
-
-function ajaxPost(url, data) {
-	return new Promise(function (resolve, reject) {
-		var xhr = new XMLHttpRequest;
-		xhr.open('POST', url);
+		xhr.open(method, url);
 		xhr.setRequestHeader('Content-Type', 'application/json');
 		xhr.send(JSON.stringify(data));
 		xhr.onload = function () {
@@ -28,3 +15,6 @@ function ajaxPost(url, data) {
 		xhr.send();
 	});
 }
+
+ajax.get = function (url) { return ajax('GET', url);
+ajax.post = function (url, data) { return ajax('POST', url, data);
