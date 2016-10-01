2e931273c5f1fcf44e38c0201270365e03086c5d
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-10-02 00:58:42 +0900

    ajax

--- public/js/ajax-xhr.js
+++ public/js/ajax-xhr.js
@@ -5,16 +5,23 @@ function ajax(method, url, data) {
 	return new Promise(function (resolve, reject) {
 		var xhr = new XMLHttpRequest;
 		xhr.open(method, url);
-		xhr.setRequestHeader('Content-Type', 'application/json');
-		xhr.send(JSON.stringify(data));
+		xhr.setRequestHeader('X-Get-Data', 'true');
 		xhr.onload = function () {
-			if (xhr.status == 200) resolve(xhr.response);
+			if (xhr.status == 200) {
+				var body = xhr.response;
+				try { body = JSON.parse(body); } catch (e) {}
+				resolve({body: body});
+			}
 			else reject(new Error(xhr.statusText));
 		}
 		xhr.onerror = reject;
-		xhr.send();
+		if (typeof data !== 'undefined') {
+			xhr.setRequestHeader('Content-Type', 'application/json');
+			xhr.send(JSON.stringify(data));
+		}
+		else xhr.send();
 	});
 }
 
-ajax.get = function (url) { return ajax('GET', url); };
+ajax.get  = function (url)       { return ajax('GET',  url); };
 ajax.post = function (url, data) { return ajax('POST', url, data); };
