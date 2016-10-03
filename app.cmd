bc7f3f79b20cd1945f0cacac19a5037fae844ad3
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-10-04 07:27:31 +0900

    revise

--- app.cmd
+++ app.cmd
@@ -1,7 +1,8 @@
 @rem set DEBUG=app:*,express:*
-@set DEBUG=app:*,express:application,express:router
+@rem set DEBUG=app:*,express:application,express:router
 set DEBUG=app:*,express:application
 node tools/prestart
-start npm start
+start gulp
+start node app
 start http://localhost:3000
 pause
