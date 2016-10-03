bad883a0e1c93033d7078e20365594cca011ee4c
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-10-04 08:35:04 +0900

    revise gulp etc

--- env-build.cmd
+++ env-build.cmd
@@ -1,3 +1,4 @@
+call npm i -g express-generator browserify gulp
 call npm i -D react react-dom browserify babelify vinyl-source-stream gulp-plumber gulp-uglify
 call npm i -D babel-preset-react babel-preset-es2015
 call npm i -D gulp
