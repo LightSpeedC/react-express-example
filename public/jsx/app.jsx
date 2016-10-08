bf93c71eb8b2e5b966f3bf3ef071f7588d45dd1a
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-10-09 07:38:15 +0900

    react router, header, footer

--- public/jsx/app.jsx
+++ public/jsx/app.jsx
@@ -3,6 +3,7 @@
 // Reactをインポート
 import {Component} from 'react';
 import {render} from 'react-dom';
+import {Router, Route} from 'react-router';
 
 import {Button, Alert, Spinner} from 'elemental';
 
@@ -12,10 +13,17 @@ import User from './user.jsx';
 import Users from './users.jsx';
 import Timer from './timer.jsx';
 
+const Header = props => <header>header</header>;
+
+const Main = props => <main>main</main>;
+
+const Footer = props => <footer>footer</footer>;
+
 // コンポーネント
 class App extends Component {
 	render() {
 		return <div>
+			<Header />
 			<h1 key="h1">React.jsのテスト</h1>
 			<HelloMessage key="hm" name="React!" />
 			<SocketNews key="sn" socket={socket} />
@@ -26,11 +34,17 @@ class App extends Component {
 			</table>*/}
 			<div>{this.props.children}</div>
 			<Button>xx</Button>
+			<Footer />
 		</div>;
 	}
 }
 
 // レンダリング
-render(<App />, document.getElementById('app'));
+render(<Router>
+		<Route path="/" component={App} />
+	</Router>,
+	document.getElementById('app'));
 
-// 参考URL https://www.willstyle.co.jp/blog/268/
+// 参考URL
+// https://www.willstyle.co.jp/blog/268/ - [入門] React.jsさわってみた。
+// http://qiita.com/momunchu/items/dcd95d186ac3b74d1edf - 私のReact用SPA向けgulpfile.js
