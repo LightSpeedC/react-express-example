69af9cf76cd014624a59b4b109215483f8490fb0
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-10-14 08:38:56 +0900

    merge

--- gulpfile.js
+++ gulpfile.js
@@ -2,33 +2,55 @@
 setTimeout(require, 3000, './app');
 
 // gulpfile.js
-var gulp       = require('gulp'),
-	plumber    = require('gulp-plumber'),
-	uglify     = require('gulp-uglify'),
-	browserify = require('browserify'),
-	babelify   = require('babelify'),
-	source     = require('vinyl-source-stream');
+const gulp       = require('gulp');
+const plumber    = require('gulp-plumber');
+const uglify     = require('gulp-uglify');
+const browserify = require('browserify');
+const babelify   = require('babelify');
+const source     = require('vinyl-source-stream');
+//const reactify   = require('reactify');
+const literalify = require('literalify');
 
 // トランスパイル
-gulp.task('browserify', function () {
+// browserify
+gulp.task('browserify', () =>
 	browserify('./public/jsx/app.jsx', {debug: true})
+		//.transform(reactify)
 		.transform(babelify, {presets: ['es2015', 'react']})
+		//.transform(babelify, {presets: ['react']})
+		//.transform(babelify.configure({
+		//	presets: ['es2015', 'react']
+		//}))
+		.transform(literalify.configure({
+			'react': 'window.React',
+			'react-dom': 'window.ReactDOM',
+			'react-router': 'window.ReactRouter'
+		}))
 		.bundle()
-		.on('error', function (err) { console.log('Error : ' + err.message); })
+		.pipe(plumber())
+		//.on('error', function (err) { console.log('Error : ' + err.message); })
 		.pipe(source('bundle.js'))
 		.pipe(gulp.dest('./public/js/'))
+/*
 		.on('end', function () {
 			gulp.src(['./public/js/bundle.js'])
 				.pipe(plumber())
 				.pipe(uglify({mangle: false}))
 				.pipe(gulp.dest('./public/js/min'))
 		})
-});
+*/
+);
+
+// copy-min-js
+gulp.task('copy-min-js', () =>
+	gulp.src(['./node_modules/react/dist/*.min.js',
+			'./node_modules/react-dom/dist/*.min.js',
+			'./node_modules/react-router/umd/*.min.js'])
+		.pipe(gulp.dest('./public/js/min')));
 
 // watch 
-gulp.task('watch', function () {
-	gulp.watch('./public/jsx/*.jsx', ['browserify']);
-});
+gulp.task('watch', () =>
+	gulp.watch('./public/jsx/*.jsx', ['browserify']));
 
 // default
-gulp.task('default', ['browserify', 'watch']);
+gulp.task('default', ['copy-min-js', 'browserify', 'watch']);
