aed06357dfe5c268c9764b42c3bcfa0ff586373d
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-10-15 00:29:16 +0900

    yarn

--- app.cmd
+++ app.cmd
@@ -2,7 +2,8 @@
 @rem set DEBUG=app:*,express:application,express:router
 set DEBUG=app:*,express:application
 set NODE_ENV=development
-node tools/prestart
+call yarn
+@rem node tools/prestart
 start gulp
 @rem start node app
 start http://localhost:3000
